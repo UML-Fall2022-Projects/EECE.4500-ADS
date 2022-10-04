4a184d9c1f720a9315a2941865792525e7717c86
Patrick Sullivan <psullivang7@gmail.com>
2022-10-04 13:12:52 -0400

    Add counter entity and start ro_puf

--- Project 1/Quartis/project_pkg.vhd
+++ Project 1/Quartis/project_pkg.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.math_real.all;
 
 package project_pkg is
 	---- component declarations
@@ -14,6 +15,20 @@ package project_pkg is
 		);
 	end component ring_oscillator;
 	
+	--counter
+	component counter is
+		generic(
+			count_size: positive := 8
+		);
+
+		port(
+			count: in std_logic;
+			reset: in std_logic;
+			enable: in std_logic;
+			q: out std_logic_vector(8 - 1 downto 0)
+		);
+	end component counter;
+	
 	--ro_puf
 	component ro_puf is
 		generic (
@@ -23,8 +38,8 @@ package project_pkg is
 		port (
 			reset: in std_logic;
 			enable: in std_logic;
-			challenge: in std_logic_vector(0 to LOG(2, ro_count) - 1);
-			response: out std_logic;
+			challenge: in std_logic_vector(0 to positive(ceil(log2(real(ro_count)))) - 1);
+			response: out std_logic
 		);
 	end component ro_puf;
 end package;
--- Project 1/Quartis/ro_puf.vhd
+++ Project 1/Quartis/ro_puf.vhd
@@ -1,6 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.math_real.all;
+use ieee.numeric_std.all;
 
 library work;
 use work.project_pkg.all;
@@ -13,13 +14,51 @@ entity ro_puf is
 	port (
 		reset: in std_logic;
 		enable: in std_logic;
-		challenge: in std_logic_vector(0 to LOG(2, ro_count) - 1);
-		response: out std_logic;
+		challenge: in std_logic_vector(0 to positive(ceil(log2(real(ro_count)))) - 1);
+		response: out std_logic
 	);
 end entity ro_puf;
 
-architecture rtl of ro_puf is
-
+architecture ro_puf1 of ro_puf is
+	type arr_std_logic is array (natural range <>) of std_logic_vector(0 to 7);
+	signal counters_out: arr_std_logic(0 to ro_count - 1);
+	signal counters_in: std_logic_vector(0 to ro_count - 1);
+		
+	function power_of_two(num: std_logic_vector) return std_logic is
+		variable total_bits: integer := 0;
+		variable iters: integer := 0;
+	begin
+		for i in num'range loop
+			iters := iters + 1;
+			if num(i) = '1' then
+				total_bits := total_bits + 1;
+			end if;
+		end loop;
+		if total_bits = 1 then
+			return '1';		
+		end if;
+		return '0';
+	end function;
+	constant ro_count_size: integer := integer(ceil(log2(real(ro_count))));
 begin
-
-end architecture rtl;
+	-- Ensure ro_count is a power of two (note: does not work as of now)
+	--assert power_of_two(std_logic_vector(to_unsigned(ro_count, ro_count_size))) = '1' report "invalid ro count" severity error;
+	
+	counters: for stage in counters_out'range generate
+		u0: counter
+			port map (
+				count => counters_in(stage),
+				reset => reset,
+				enable => enable,
+				q => counters_out(stage)
+			);
+	end generate counters;
+	
+	ring_oscillators: for oscillator in counters_in'range generate
+		u0: ring_oscillator
+			port map (
+				enable => enable,
+				osc_out => counters_in(oscillator)
+			);
+	end generate ring_oscillators;
+end architecture ro_puf1;
