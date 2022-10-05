aee0c9963d0b23e2940528ac71e93001431d3dfb
Patrick Sullivan <psullivang7@gmail.com>
2022-10-05 17:31:55 -0400

    Fix assert

--- Project 1/Quartis/ro_puf.vhd
+++ Project 1/Quartis/ro_puf.vhd
@@ -23,26 +23,26 @@ architecture ro_puf1 of ro_puf is
 	type arr_std_logic is array (natural range <>) of std_logic_vector(0 to 7);
 	signal counters_out: arr_std_logic(0 to ro_count - 1);
 	signal counters_in: std_logic_vector(0 to ro_count - 1);
+	signal top_out: std_logic_vector() := 0;
+	signal bottom_out: std_logic_vector() := 0;
 		
-	function power_of_two(num: std_logic_vector) return std_logic is
+	function power_of_two(num: in natural) return boolean is
 		variable total_bits: integer := 0;
 		variable iters: integer := 0;
+		constant num_bits: integer := integer(ceil(log2(real(num))));
+		constant num_vec: std_logic_vector := std_logic_vector(to_unsigned(num, num_bits));
 	begin
-		for i in num'range loop
+		for i in num_vec'range loop
 			iters := iters + 1;
-			if num(i) = '1' then
+			if num_vec(i) = '1' then
 				total_bits := total_bits + 1;
 			end if;
 		end loop;
-		if total_bits = 1 then
-			return '1';		
-		end if;
-		return '0';
+		return total_bits = 0;
 	end function;
-	constant ro_count_size: integer := integer(ceil(log2(real(ro_count))));
 begin
 	-- Ensure ro_count is a power of two (note: does not work as of now)
-	--assert power_of_two(std_logic_vector(to_unsigned(ro_count, ro_count_size))) = '1' report "invalid ro count" severity error;
+	assert power_of_two(ro_count) = true report "invalid ro count" severity error;
 	
 	counters: for stage in counters_out'range generate
 		u0: counter
