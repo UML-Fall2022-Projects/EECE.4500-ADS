57c858ae5fef8b7247ea58a866c180cd30248cae
beewyka819 <psullivang7@gmail.com>
2022-10-25 17:41:12 -0400

    Fix coords not propagating properly in mandelbrot gen

--- Project 2/Quartus/mandelbrot_gen.vhd
+++ Project 2/Quartus/mandelbrot_gen.vhd
@@ -28,19 +28,20 @@ end entity mandelbrot_gen;
 architecture mandel_gen of mandelbrot_gen is
 	type complex_array is array (natural range<>) of ads_complex;
 	signal f_of_z: complex_array(0 to iterations) := (others => ads_cmplx(to_ads_sfixed(0), to_ads_sfixed(0)));
+	signal coords_list: complex_array(0 to iterations) := (others => ads_cmplx(to_ads_sfixed(0), to_ads_sfixed(0)));
 	
 	signal thresholds: std_logic_vector(0 to iterations) := (others => '0');
 	
 	type iterated_array is array (natural range<>) of natural;
 	signal iteration_signal: iterated_array(0 to iterations) := (others => 0);
-	
 begin
 	
 	process(system_clock) is
 	begin
-		if rising_edge(system_clock) then				
+		if rising_edge(system_clock) then
 			for i in iterations downto 1 loop
-				f_of_z(i) <= f_of_z(i - 1) * f_of_z(i - 1) + coords;
+				f_of_z(i) <= f_of_z(i - 1) * f_of_z(i - 1) + coords_list(i);
+				coords_list(i) <= coords_list(i - 1);
 				if thresholds(i - 1) = '1' then
 					thresholds(i) <= thresholds(i - 1);
 					iteration_signal(i) <= iteration_signal(i - 1);
@@ -53,7 +54,7 @@ begin
 					end if;
 				end if;
 			end loop;
-			
+			coords_list(0) <= coords;
 			index_o <= iteration_signal(iterations);
 		end if;
 	end process;
